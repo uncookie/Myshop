5e5778ce24d7fb62dae5fb1374d529cfd371ffe2
unacookie <unapony@gmail.com>
2019-08-14 10:19:11 +0800

    建立後台

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-
 <h1>商品一覽</h1>
+@endsection
+
+@section('my_menu')
+<li class="nav-item">
+    @parent
+    <a class="nav-link" href="/home">回控制台</a>
+</li>
 @endsection
